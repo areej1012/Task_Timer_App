a7ed5127d68c35477715595f468a202ce3a001a9
arwaalmashi <ar.al-mashi@hotmail.com>
2022-01-06 07:05:55 +0300

    Get Started Ready

--- app/src/main/java/com/example/tasktimerapp/fragment/Slide4Fragment.kt
+++ app/src/main/java/com/example/tasktimerapp/fragment/Slide4Fragment.kt
@@ -2,13 +2,15 @@ package com.example.tasktimerapp.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tasktimerapp.R
 import com.example.tasktimerapp.activity.TaskListActivity
-import kotlinx.android.synthetic.main.test.*
+import kotlinx.android.synthetic.main.test.get_started_button
+import kotlinx.android.synthetic.main.test.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,14 +34,27 @@ class Slide4Fragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
 
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slide4, container, false)
+        //return inflater.inflate(R.layout.fragment_slide4, container, false)
+
+        val view: View = inflater!!.inflate(R.layout.fragment_slide4, container, false)
+
+        view.get_started_button.setOnClickListener { view ->
+            Log.d("btnSetup", "Selected")
+            val intent = Intent (activity, TaskListActivity::class.java)
+            activity?.startActivity(intent)
+        }
+
+        // Return the fragment view/layout
+        return view
     }
 
     companion object {
