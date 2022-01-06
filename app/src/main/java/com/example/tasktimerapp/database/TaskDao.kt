8365658b9ea02f8915eed99e30cdfc6a07eeed14
saadalajlan23 <48617522+saadalajlan23@users.noreply.github.com>
2022-01-06 13:11:35 +0300

    add update

--- app/src/main/java/com/example/tasktimerapp/database/TaskDao.kt
+++ app/src/main/java/com/example/tasktimerapp/database/TaskDao.kt
@@ -6,6 +6,7 @@ import androidx.room.*
 @Dao
 interface TaskDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun addPerson(person: Task)
-
+    fun addTask(task: Task)
+    @Update
+    suspend fun updateTask(task: Task)
 }
